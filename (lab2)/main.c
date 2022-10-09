f3fab86247d73ed6542dcd81414241270893d3ef
Artur229322 <229322@edu.p.lodz.pl>
2022-10-09 12:10:33 +0200

    Dodanie funkcjonalności #k

--- (lab2)/main.c
+++ (lab2)/main.c
@@ -4,16 +4,24 @@
 
 
 int my_printf(char *format_string, char *param){
+
+
 	for(int i=0;i<strlen(format_string);i++){
-			for(int i=0;i<strlen(format_string);i++){
-				if((format_string[i] == '#') && (format_string[i+1] == 'k')){
-					i++;
-					printf("%s",param);
-				}else
-					putchar(format_string[i]);
+		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
+				for(int j=0;j<strlen(format_string);j++){
+					if(isupper(format_string[j])>0){
+						format_string[j] = tolower(format_string[j]);
+					}else
+						format_string[j] = toupper(format_string[j]);
 				}
+			i++;
+			printf("%s",param);
+		}else
+			putchar(format_string[i]);
 	}
+
 	puts("");
+	return 0;
 }
 
 int main(int argc, char *argv[]){
