720af4b1ecd1561baeb869b0ff367a091c6f5855
Artur229322 <229322@edu.p.lodz.pl>
2022-11-06 11:26:54 +0100

    Poprawki zwracanych parametrów w funkcji print_digits()

--- lab4/main.py
+++ lab4/main.py
@@ -4,12 +4,12 @@ import sys
 
 def print_digits(format_string,index):
     buffer=""
-    for i in range (index+2,len(format_string)):
+    for i in range (index+3,len(format_string)):
         if format_string[i].isdigit():
             buffer += format_string[i]
         else:
-            return buffer   
-    return '0'
+            return buffer[::-1],i   
+    return "",index
 
 def my_printf(format_string,param):
     #print(format_string)
@@ -18,8 +18,9 @@ def my_printf(format_string,param):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1]=='g':
                 print(param,end="")
-                if(print_digits(format_string,idx)!='0'):
-                    print(print_digits(format_string,idx)[::-1],end="")
+                buffer,idx=print_digits(format_string,idx)
+                if(buffer!=""):
+                    print(" "+buffer,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
