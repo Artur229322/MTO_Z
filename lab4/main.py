211b6a3c6c7e707ee38c5bc2cf3e392539e033fa
Artur229322 <229322@edu.p.lodz.pl>
2022-11-06 11:16:45 +0100

    Stworzenie funkcji print_digits

--- lab4/main.py
+++ lab4/main.py
@@ -2,19 +2,33 @@
 
 import sys
 
+def print_digits(format_string,index):
+    buffer=""
+    for i in range (index+2,len(format_string)):
+        if format_string[i].isdigit():
+            buffer += format_string[i]
+        else:
+            return buffer   
+    return '0'
+
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
+            if format_string[idx] == '#' and format_string[idx+1]=='g':
                 print(param,end="")
+                if(print_digits(format_string,idx)!='0'):
+                    print(print_digits(format_string,idx)[::-1],end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
         else:
             shouldDo=True
     print("")
+
+
+
 
 data=sys.stdin.readlines()
 
