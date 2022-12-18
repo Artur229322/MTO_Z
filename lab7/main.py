ff5aa52e89682282ed3b28b1a97496155d19f158
Artur229322 <229322@edu.p.lodz.pl>
2022-12-18 10:42:25 +0100

    Stworzenie szkieletu funkcji do zamiany na kod szesnastkowy

--- lab7/main.py
+++ lab7/main.py
@@ -2,12 +2,34 @@
 
 import sys
 
+def fexPrint(format_string):
+    dict1 = {
+        "1":"a",
+        "2":"b",
+        "3":"c",
+        "4":"d",
+        "5":"e",
+        "6":"f"}
+    dict2 = {
+        "1":"g",
+        "2":"h",
+        "3":"i",
+        "4":"j",
+        "5":"k",
+        "6":"l",}
+    
+    
+    
+    
+    
+
+
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
+            if format_string[idx] == '#' and format_string[idx+1] == 'j':
                 print(param,end="")
                 shouldDo=False
             else:
