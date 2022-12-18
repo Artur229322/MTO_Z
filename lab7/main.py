0d24393e69f5c346e803613640b96fd9b1ee1e46
Artur229322 <229322@edu.p.lodz.pl>
2022-12-18 10:54:19 +0100

    Rozdzielenie funkcjonalności na mniejsze funkcje.

--- lab7/main.py
+++ lab7/main.py
@@ -2,24 +2,33 @@
 
 import sys
 
-def fexPrint(format_string):
+def hexify(format_string):
+    hexString = []
+    
+    for i in range(0,len(format_string)):
+        hexString.append(hex(format_string[i]))
+        
+def changeAlphabetLetters(hexString):    
     dict1 = {
-        "1":"a",
-        "2":"b",
-        "3":"c",
-        "4":"d",
-        "5":"e",
-        "6":"f"}
+            "1":"a",
+            "2":"b",
+            "3":"c",
+            "4":"d",
+            "5":"e",
+            "6":"f"}
     dict2 = {
-        "1":"g",
-        "2":"h",
-        "3":"i",
-        "4":"j",
-        "5":"k",
-        "6":"l",}
-    
-    
-    
+            "1":"g",
+            "2":"h",
+            "3":"i",
+            "4":"j",
+            "5":"k",
+            "6":"l"}
+        
+    for i in range(0,len(hexString)):
+        if(hexString[i] in dict1.values()):
+            hexString[i] = dict2.get(hexString[i])
+        elif (hexString[i] in dict2.values()):
+            hexString[i] = dict1.get(hexString[i])
     
     
 
